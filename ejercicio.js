50dc765823b41fdb9c5adc14302da2735c25d8c7
Luis Franco <lfranco21@users.noreply.github.com>
2023-01-08 11:25:45 -0500

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,19 @@
+const nombre = "Luis Franco";
+console.log(nombre);
+
+const edad = 29;
+console.log(edad);
+
+const eres_desarrollador = true;
+console.log(eres_desarrollador);
+
+const fecha_nacimiento = new Date("August 21 1993");
+console.log(fecha_nacimiento);
+
+const libro_favorito = {
+    titulo: "El principito",
+    autor: "Antoine de Saint-Exupery",
+    fecha:"1943"
+}
+
+console.log(libro_favorito.autor);
